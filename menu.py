e56d3bdd38bbb0eeda6925e9a1ac23f681ccccc1
“An <andd.fhs@gmail.com>
2023-06-21 18:53:45 +1000

    Use list for the menu and price

--- menu.py
+++ menu.py
@@ -1,33 +1,30 @@
+tea_menu = ["Classic Milk Tea", "Thai Milk Tea", "Honey Milk Tea", "Honeydew Milk Tea", 
+            "Mango Milk Tea", "Peach Milk Tea", "Jasmine Milk Tea", "Taro Milk Tea",
+            "Matcha Milk Tea", "Ginger Milk Tea", "Guava Milk Tea", "Passion Fruit Milk Tea"]
+
+tea_prices = [5.50, 5.50, 5.75, 6.00, 6.00, 6.00, 6.25, 6.50, 6.50, 6.50, 6.50, 6.75, 6.75]
+ 
 def welcome_messages():
     print("=======================================")
     print("     WELCOME TO X BUBBLE MILK TEA")
     print("=======================================")
 
-    print("Pick your favourite milk tea from our menu ^^")
+    print("Pick your favourite milk tea from our menu ^^: ")
+    print()
 
 def menu():
-    print("1. Classic Milk Tea")
-    print("2. Thai Milk Tea")
-    print("3. Honey Milk Tea")
-    print("4. Honeydew Milk Tea")
-    print("5. Mango Milk Tea")
-    print("6. Peach Milk Tea")
-    print("7. Jasmine Milk Tea")
-    print("9. Taro Milk Tea")
-    print("10. Matcha Milk Tea")
-    print("11. Ginger Milk Tea")
-    print("12. Guava Milk Tea")
-    print("13. Passion Fruit Milk Tea")
-
+  for i in range(0, len(tea_menu)):
+      print(tea_menu[i])
+      
 def selection():
     menu()
     print()
-    choice = int(input("Your drink choice: "))
+    choice = int(input("Your tea choice: "))
     if choice > 0 and choice <= 13:
         quantity()
 
 def quantity():
-    print("How many drink would you like?")
+    drink_quantity = int(input("How many would you like? "))
 
 if __name__ == "__main__":
     welcome_messages()
