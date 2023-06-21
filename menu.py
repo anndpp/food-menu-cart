5799b6a81231679a61537c42cad16d4d4f761167
“An <andd.fhs@gmail.com>
2023-06-21 20:00:25 +1000

    Delete unused function and whitespaces

--- menu.py
+++ menu.py
@@ -2,8 +2,6 @@ tea_menu = ["1. Classic Milk Tea", "2. Thai Milk Tea", "3. Honey Milk Tea", "4. 
             "5. Mango Milk Tea", "6. Peach Milk Tea", "7. Jasmine Milk Tea", "8. Taro Milk Tea",
             "9. Matcha Milk Tea", "10. Ginger Milk Tea", "11. Guava Milk Tea", "12. Passion Fruit Milk Tea"]
 
-
-
 tea_prices = [5.50, 5.50, 5.75, 6.00, 6.00, 6.00, 6.25, 6.50, 6.50, 6.50, 6.50, 6.75, 6.75]
  
  # display welcome message
@@ -34,12 +32,9 @@ def selection():
     if user_choice in tea_list_numbers:
         idx = tea_list_numbers.index(user_choice)
         price = tea_prices[idx]
-        print("Your milk tea costs {}".format(price))
+        print("Your milk tea costs ${}".format(price))
     else:
         print("Tea not in the menu")
-
-def quantity():
-    drink_quantity = int(input("How many would you like? "))
 
 if __name__ == "__main__":
     welcome_messages()
